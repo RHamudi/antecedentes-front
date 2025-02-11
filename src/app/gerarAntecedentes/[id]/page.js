0708737e0b07b5a5ed90e376c55d86e8ed4c8ab8
RHamudi <ramonramos.silva16@gmail.com>
2025-02-11 15:49:42 -0300

    feat: login system

--- src/app/gerarAntecedentes/[id]/page.js
+++ src/app/gerarAntecedentes/[id]/page.js
@@ -1,11 +1,33 @@
+"use client";
+
 import Dashboard from "@/components/dashboard";
 import Sidebar from "@/components/sidebar";
+import { useAppContext } from "@/context/context";
+import withAuth from "@/context/withAuth";
+import { useRouter } from "next/navigation";
+import { use, useEffect } from "react";
 
-export default function gerar({ params }) {
+function gerar({ params }) {
+  const { id } = use(params);
+  const { auth } = useAppContext();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!auth) {
+      router.push("/login"); // Redireciona para login
+    }
+  }, [auth, router]);
+
+  if (!auth) {
+    return null; // Pode exibir um loading aqui se quiser
+  }
+
   return (
     <main className="flex flex-row w-screen h-screen">
       <Sidebar />
-      <Dashboard id={params.id} />
+      <Dashboard id={id} />
     </main>
   );
 }
+
+export default withAuth(gerar);
