f67b8d4a467dd636fae1e868c1b30ef820f9d04e
RHamudi <ramonramos.silva16@gmail.com>
2025-02-11 15:49:03 -0300

    feat: create contextapi

---
+++ src/context/context.js
@@ -0,0 +1,46 @@
+"use client"; // Context precisa rodar no client
+
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import { createContext, useContext, useState } from "react";
+
+// Cria o contexto
+const AppContext = createContext();
+
+// Criar Provider
+export function AppProvider({ children }) {
+  const [auth, setAuth] = useState(false);
+  const [nome, setNome] = useState("");
+  const [userId, setUserId] = useState();
+  const route = useRouter();
+
+  function Authenticate(email, senha) {
+    axios
+      .post(`http://127.0.0.1:8000/login`, {
+        email: email,
+        senha: senha,
+      })
+      .then((e) => {
+        setAuth(true);
+        setNome(e.name);
+        setUserId(e.id);
+        route.push(`/gerarAntecedentes/${e.data.idUser}`);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <AppContext.Provider value={{ auth, nome, userId, Authenticate }}>
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+// Hook para facilitar o acesso ao contexto
+export function useAppContext() {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error("useAppContext deve ser usado dentro de um AppProvider");
+  }
+  return context;
+}
