bcc57338283935c53c7370de4c2c2d0b26796704
RHamudi <ramonramos.silva16@gmail.com>
2025-02-11 15:52:09 -0300

    feat: styles page login

--- src/app/page.js
+++ src/app/page.js
@@ -1,50 +1,43 @@
 "use client";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+
+import { useAppContext } from "@/context/context";
 import { useState } from "react";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
-  const route = useRouter();
-
-  function Authenticate() {
-    axios
-      .post(`http://127.0.0.1:8000/login`, {
-        email: email,
-        senha: senha,
-      })
-      .then((e) => {
-        route.push(`/gerarAntecedentes/${e.data.idUser}`);
-      })
-      .catch((err) => console.log(err));
-  }
+  const { Authenticate } = useAppContext();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center bg-gray-900 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1>Login</h1>
+    <div className="grid grid-rows-[20px_1fr_20px] items-center bg-gray-100 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center px-[150] py-[80] shadow-xl rounded bg-white text-black">
+        <h1 className="font-bold text-3xl">Login</h1>
         <label title="Email">
-          <h2>Email</h2>
+          <h2 className="font-medium">Email</h2>
           <input
             id="Email"
             type="text"
             value={email}
-            className="text-black"
+            className="text-black p-1 border-2 border-solid shadow-xl rounded"
             onChange={(e) => setEmail(e.target.value)}
           />
         </label>
         <label title="senha">
-          <h2>Senha</h2>
+          <h2 className="font-medium">Senha</h2>
           <input
             id="senha"
-            type="text"
-            className="text-black"
+            type="password"
+            className="text-black p-1 border-2 border-solid shadow-xl rounded"
             value={senha}
             onChange={(e) => setSenha(e.target.value)}
           />
         </label>
-        <button onClick={Authenticate}>Login</button>
+        <button
+          className="border-2l rounded border-solid bg-blue-700 text-white px-10 py-2"
+          onClick={() => Authenticate(email, senha)}
+        >
+          Login
+        </button>
       </main>
     </div>
   );
