409c67215c184e41d54f6cd3bc7fcc2c5c2949f2
RHamudi <ramonramos.silva16@gmail.com>
2025-02-20 16:14:55 -0300

    fix: styles in login

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center bg-gray-100 justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center px-[150] py-[80] shadow-xl rounded bg-white text-black">
+      <main className="flex flex-col gap-8 row-start-2 items-center px-32 py-24 shadow-xl rounded bg-white text-black">
         <h1 className="font-bold text-3xl">Login</h1>
         <label title="Email">
           <h2 className="font-medium">Email</h2>
